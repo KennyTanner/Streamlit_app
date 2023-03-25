a0fb4fd9d2e5966e9a93574fd1c8d8b8b0c4200d
KennyTanner <Kennybtanner@gmail.com>
2023-03-25 12:06:11 -0700

    Update streamlit_app.py
    added a new header section to 'build own'

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boilded Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
