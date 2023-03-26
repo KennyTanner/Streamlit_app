e1fc245d5eddb09d5e36b189d478f380731956de
KennyTanner <Kennybtanner@gmail.com>
2023-03-26 13:17:33 -0700

    showing data from response in df

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# streamlit.text(fruityvice_response.json()) # as dataframe instead
+# normalize first
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
